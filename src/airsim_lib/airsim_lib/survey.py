6c4126b4eeda4235acd7bc53fcc133b6c01aa8c3
Shreyas Shandilya <shreyas.shandilya@ideaforgetech.com>
2023-12-18 22:34:17 +0530

    Testing:AirSim Setup and Data Acquisition Pipeline
    Problem
    =======
    1. Logging of GPS data from AirSim not implemented
    2. GPU dependence of AirSim bottleneck in development
    
    Solution
    ========
    1. Implemented `def logToFile(obs)` to log all gps observations into a
    text file in SkyNetv3 compatible format
    2. Implemented interface for running AirSim Blocks environment, a simple
    test environment which can run on CPU
    
    Note
    ====
    The data acquisition from GPS has been testing only with Blocks
    environment. Need to test with high end photo realistic environments

--- src/airsim_lib/airsim_lib/survey.py
+++ src/airsim_lib/airsim_lib/survey.py
@@ -1,4 +1,6 @@
+import os
 import airsim
+import copy
 import numpy as np
 import cv2
 import rclpy
@@ -10,15 +12,25 @@ from sensor_msgs.msg import NavSatFix
 from geometry_msgs.msg import Vector3Stamped
 import cv_bridge
 
+AIRCRAFT_OFF = 0
+AIRCRAFT_STARTUP = 13
+AIRCRAFT_TAKEOFF = 6
+AIRCRAFT_LAND = 7
+AIRCRAFT_HOME = 3
+AIRCRAFT_HOVER = 11
+
 class SurveyNavigator:
     dist_threshold = 10
     def __init__(self, logdir, num_waypoints, show = True):
+        self.frame_counter = 0
         self.num_waypoints = num_waypoints
+        self.currentUAVMode = AIRCRAFT_OFF
         self.show = show
         self.descend = False
         self.descent_started = False
         self.logdir = logdir
         self.client = airsim.MultirotorClient()
+        self.currentUAVMode = AIRCRAFT_STARTUP
         self.client.confirmConnection()
         self.client.enableApiControl(True)
         state = self.client.getMultirotorState()
@@ -34,19 +46,26 @@ class SurveyNavigator:
 
     def start(self, step_callback):
         self.client.armDisarm(True)
+        for i in range(40):
+            obs = self._get_observation()
+            step_callback(obs)
         state = self.client.getMultirotorState()
         landed = state.landed_state
         self.start = state.kinematics_estimated.position
         if(landed == airsim.LandedState.Landed):
+            self.currentUAVMode = AIRCRAFT_TAKEOFF
             self.takeoff = True
             print("taking off...")
-            self.client.takeoffAsync().join()
+            self.client.takeoffAsync()
             state = self.client.getMultirotorState()
             self.start = state.kinematics_estimated.position
             pos = state.kinematics_estimated.position
             gps = state.gps_location
             print("position: x: {} y:{} z:{}".format(pos.x_val, pos.y_val, pos.z_val))
             print("gps position: altitude: {} latitude: {} longitude: {}".format(gps.altitude, gps.latitude, gps.longitude))
+        for i in range(40):
+            obs = self._get_observation()
+            step_callback(obs)
        
         self.set_path()
 
@@ -79,6 +98,8 @@ class SurveyNavigator:
             if dist < 2:
                 if not self.descent_started:
                     print("Reached waypoint ", index)
+                if index == 0:
+                    self.currentUAVMode = AIRCRAFT_HOVER
                 if index >= 0:
                     index += 1 
                 if index > self.waypoints.shape[0] - 1 and index >= 0:
@@ -86,14 +107,21 @@ class SurveyNavigator:
                     goal = self.waypoints[0]
                     index = -1
                 elif index < 0 and not self.descent_started:
+                    self.currentUAVMode = AIRCRAFT_LAND
                     print("Landing Sequence Starting")
                     self.descent_started = True
                     goal = np.array([self.start.x_val, self.start.y_val, self.start.z_val])
                 elif index < 0 and self.descent_started and not self.descend:
                     print("Landing")
+                    obs['mode'] = AIRCRAFT_HOME
+                    obs = self._get_observation()
+                    step_callback(obs)
                     self.descend = True
                 elif index < 0 and self.descent_started and self.descend:
                     count+=1
+                    if(count % 10 == 0):
+                        obs = self._get_observation()
+                        step_callback(obs)
                     if(count > 500):
                         print("Breaking Loop")
                         break
@@ -108,11 +136,17 @@ class SurveyNavigator:
             print("position: x: {} y:{} z:{}".format(pos.x_val, pos.y_val, pos.z_val))
             print("gps position: altitude: {} latitude: {} longitude: {}".format(gps.altitude, gps.latitude, gps.longitude))
             self.client.landAsync().join()
+            obs = self._get_observation()
+            step_callback(obs)
             #state = self.client.getMultirotorState()
             #print("position: ", state.kinematics_estimated.position)
 
             print("disarming.")
             self.client.armDisarm(False)
+            for i in range(20):
+                obs['mode'] = AIRCRAFT_OFF
+                obs = self._get_observation()
+                step_callback(obs)
 
     def set_path(self):
         print("Setting Path for Sampling")
@@ -156,12 +190,13 @@ class SurveyNavigator:
             airsim.ImageRequest("3", airsim.ImageType.Scene, False, False),
             airsim.ImageRequest("3", airsim.ImageType.DepthVis, False, False),
             ])
+        state = self.client.getMultirotorState()
         gnss = self.client.getGpsData().gnss
-        #print(self.client.getGpsData().gnss)
+        gps = gnss.geo_point
+        pos = state.kinematics_estimated.position
+        vel = gnss.velocity
         self.client.simPause(False)
         
-        gps = gnss.geo_point
-
         response = responses[0]
         img1d = np.frombuffer(response.image_data_uint8, dtype=np.uint8)
         rgb = img1d.reshape(response.height, response.width, 3)
@@ -181,17 +216,26 @@ class SurveyNavigator:
             'scene_timestamp': rgb_timestamp, 
             'depth': depth,
             'depth_timestamp': depth_timestamp,
-            'gps': np.array([gps.latitude, gps.longitude, gps.altitude]),
+            'gps': np.array([gps.latitude, gps.longitude, -gps.altitude]),
             'gps_timestamp': gnss.time_utc,
-            'gps_vel': np.array([gnss.velocity.x_val, gnss.velocity.y_val, gnss.velocity.z_val]),
-            'gps_err_std': np.array([gnss.eph, gnss.epv])
+            'gps_vel': np.array([vel.x_val, vel.y_val, vel.z_val]),
+            'gps_err': np.array([gnss.eph, gnss.epv]),
+            'mode': copy.deepcopy(self.currentUAVMode),
+            'position': np.array([pos.x_val, pos.y_val, -pos.z_val]),
+            'frame_counter': copy.deepcopy(self.frame_counter)
         }
+        self.frame_counter += 1
         return obs
 
 
 class AirSimNode(Node):
-    def __init__(self, nodeName = "airsim_node"):
+    def __init__(self, nodeName = "airsim_node", logToFile = True):
         super().__init__(nodeName)
+        self.logToFile = logToFile
+        if self.logToFile:
+            if os.path.exists('day_gps.txt'):
+                os.remove('day_gps.txt')
+            self.file = open("day_gps.txt", 'a')
         self.bridge = cv_bridge.CvBridge()
         self.navigator = SurveyNavigator("./", 2)
         obs = self.navigator._get_observation()
@@ -234,7 +278,37 @@ class AirSimNode(Node):
         gps_vel.vector.z = obs['gps_vel'][2]
         self._publishers['gps_vel'].publish(gps_vel)
 
+        if self.logToFile:
+            self.log_to_file(obs)
+
+    def log_to_file(self, obs):
+        speed = np.sqrt(np.sum(np.square(obs['gps_vel'])))
+        heading = np.degrees(np.arctan2(obs['gps_vel'][1], obs['gps_vel'][0]))
+        sensorType = 'RGBD'
+        if(obs['position'][2] > 7.5):
+            sensorType = 'Monocular'
+        data = "{:.7f},{:.7f},{:.7f},{},0,{},{},{:.7f},{:.7f},{:.7f},{},11,0,{},GDN_PROCESS_OK,TRACKING,Sensor_OK,Sensor_OK,{},0,{:.7f},0,0\n".format(
+                obs['gps'][0],
+                obs['gps'][1],
+                obs['position'][2],
+                obs['mode'],
+                obs['gps_timestamp'],
+                obs['gps_timestamp'] // (60 * 60 * 24),
+                obs['gps'][2],
+                obs['gps_err'][0],
+                speed,
+                heading,
+                sensorType,
+                obs['frame_counter'],
+                obs['position'][2]
+                )
+        self.file.write(data)
+     
+    def close(self):
+        self.file.close()
+
 
 if __name__ == "__main__":
     nav = SurveyNavigator("./", 1)
     nav.start(step_callback)
+    nav.close()
