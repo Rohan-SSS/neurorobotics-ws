7216efb68a9bb7cb39b0782b22239e3c74521a39
Shreyas Shandilya <shreyas.shandilya@ideaforgetech.com>
2024-02-22 12:33:36 +0530

    [Testing] Automation of AirSim SITL with SkyNet and BFX
    Problem
    =======
    1. Invokation of SkyNet in SITL and Passive Data Collection are blocking
       and no response is sent till processes are completed
    2. Running SkyNet and Passive Data Collection in the background kills
       all processes
    3. No Kill Switch in API
    4. Start endpoint does not check the status of the processes being run
    5. No endpoint to check the running status of processes in Oracle
    6. Incorrent invocation command for BFX committed in git VCS
    
    Solution
    ========
    1. Introduced an environment variable called DJINN_MODE to conditionally
       start processes in the background and not kill processes if djinn is
       invoked through oracle
    2. Implemented state in djinn for keeping track of airsim environment run for
       passive data collection
    3. Added kill option for passive data collection in djinn
    4. Corrected invocation command for BFX in scripts/run-bfx.sh
    5. Implemented endpoints to check process running status in oracle
    6. Implemented endpoints to kill processes through oracle
    7. Implemented running status check and appropriate response in start
       endpoint of oracle
    
    Note
    ====
    1. To setup djinn in the current state, psutil and flask must be
       installed through pip on local host
    2. WS_PATH must be set and exported in .bashrc
    3. WS_PATH must be added to PATH to make djinn globally invocable
    4. Oracle has to be added as a startup process at every new setup of the
       regression framework

--- scripts/run-bfx.sh
+++ scripts/run-bfx.sh
@@ -2,6 +2,6 @@
 
 docker exec -it bfx_container /bin/bash -l -c "cd /ws && nohup mavlink-routerd -e 192.168.26.78:14550 127.0.0.1:14550 >> mavlink.log &"
 docker exec -it bfx_container /bin/bash -l -c "sleep 5"
-docker exec -it bfx_container /bin/bash -l -c "export BFX=\"/ws/ext/bfx/BFX\" && \${BFX}/Tools/sitl_run.sh \${BFX}/build/px4_sitl_rtps/bin/px4 none airsim iris Africa \${BFX} \${BFX}/build/px4_sitl_rtps"
+docker exec -it bfx_container /bin/bash -l -c "export BFX=\"/ws/ext/bfx/BFX\" && \${BFX}/Tools/sitl_run.sh \${BFX}/build/px4_sitl_rtps/bin/px4 none none iris none \${BFX} \${BFX}/build/px4_sitl_rtps"
 # BFX="${PWD}/ext/bfx/BFX"
 # $BFX/Tools/sitl_run.sh $BFX/build/px4_sitl_rtps/bin/px4 none airsim iris Africa $BFX $BFX/build/px4_sitl_rtps
--- skynet-oracle/oracle.py
+++ skynet-oracle/oracle.py
@@ -1,13 +1,32 @@
 import os
 from flask import Flask
 import json
+# Install psutil
+import psutil
+import time
+
+def checkIfProcessRunning(processName):
+    '''
+    Check if there is any running process that contains the given name processName.
+    '''
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False;
 
 # Refer to the following link for information about how to pass arguments to a Flask APP
 # https://stackoverflow.com/questions/73635412/flask-run-with-argument
 def create_app(ws_path):
     print("Workspace Path: ", ws_path)
+    # Need to set the following values in .bashrc to be able to use djinn in general as well.
     os.environ["PATH"] = "{}:{}".format(os.environ["PATH"], ws_path)
     os.environ["WS_PATH"] = ws_path
+    os.environ["DJINN_MODE"] = "automation"
     app = Flask(__name__)
 
     @app.route("/")
@@ -35,7 +54,41 @@ def create_app(ws_path):
         print("Starting Process: ", arg)
         print("Starting Environment: ", env)
         status = os.system('djinn start {} {}'.format(arg, env))
-        if(status == 0):
+        time.sleep(30)
+        if status != 0:
+            return json.dumps({'success':False}), 500, {'ContentType':'application/json'}
+        else:
+            status = True
+        print("Spawn Status: ", status)
+
+        if arg == "sitl":
+            status_skynet = checkIfProcessRunning("skynet")
+            print("SkyNet Status: ", status_skynet)
+            status_px4 = checkIfProcessRunning("px4")
+            print("PX4 Status: ", status_px4)
+            status = status and status_px4 and status_skynet    
+        elif arg == "airsim":
+            status_airsim_lib = checkIfProcessRunning("airsim_lib")
+            print("Airsim Lib Status: ", status_airsim_lib)
+            status = status and status_airsim_lib
+        status_env = checkIfProcessRunning(env) 
+        print("Environment Status: ", status_env)
+        status = status and status_env
+        if(status):
+            return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+        else:
+            return json.dumps({'success':False}), 500, {'ContentType':'application/json'}
+
+    @app.route("/check/<process>")
+    def check_process(process):
+        print("Checking for existence of process by name: ", process)
+        status = checkIfProcessRunning(process)
+        return json.dumps({'success':True, 'processName': process, "processRunning": status}), 200, {'ContentType':'application/json'}
+
+    @app.route("/kill/<process>")
+    def kill_process(process):
+        status = os.system('djinn kill {}'.format(process))
+        if status == 0:
             return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
         else:
             return json.dumps({'success':False}), 500, {'ContentType':'application/json'}
