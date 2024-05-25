4d696441210220186520e099e4adefcce7f64474
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2024-05-25 23:49:22 +0530

    Git Config Safe Directory Error within docker container
    Problem
    =======
    1. Git config safe.directory not set error for all git repos in the
       workspace
    2. .djinn_config contains userEmail and userName by default
    3. ros_ws build artefacts tracked by git
    
    Solution
    ========
    1. Updated scripts/docker-setup.sh to add safe directory for all
       submodules within the workspace recursively
    2. Removed email and username from .djinn_config and set file as
       assumed-changed in git index
    3. Deleted build artefacts of slam/ext projectst
    
    Note
    ====
    1. Refer to the following link for git update-index reference for
       assuming .djinn_config to be unchanged: https://stackoverflow.com/a/28497505

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -21,9 +21,21 @@ echo ""
 echo "in docker-setup.sh email: $2"
 echo "in docker-setup.sh username: $3"
 
+apply_safe_directory(){
+    echo "Applying Safe Directory command to folder: /ws/${1}${2}"
+    git config --global --add safe.directory "${1}/${2}"
+}
+
+export -f apply_safe_directory
 git submodule init
 git submodule update --recursive
-git config --global --add safe.directory /ws/ros_ws
+git submodule status | awk '{print $2}' | xargs -I {} bash -c 'apply_safe_directory "$PWD" "{}"'
+cd ros_ws
+git submodule init
+git submodule update --recursive
+git submodule status | awk '{print $2}' | xargs -I {} bash -c 'apply_safe_directory "$PWD" "{}"'
+cd ../
+
 
 git config --global user.email "$2"
 git config --global user.name "$3"
