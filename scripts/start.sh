506277af0cf1f45e098ca3f77f5304851ce153d5
shandilya1998 <shandilya.shreyas@gmail.com>
2024-08-02 22:27:50 +0530

    ORBSLAM3 monocular with Tello
    Problem
    =======
    1. ros_ws commit not updated to latest
    2. djinn bug fixes and updates for better usability required
    
    Solution
    ========
    1. Updated docker build command instructions for reorganised docker
       images for nrt
    2. Using VERSION config value to set docker image version for
       nrt_container
    3. Added djinn build docker <image> <filepath> command
    4. Removed redundant options from djinn
    5. Added option to start tello test launch file
    6. Created a separate image for libcamera dependencies and pangolin
       dependencies for orbslam3
    7. Added config files for tello rviz visualisation
    8. Added functional tello orbslam3 commit in ros_ws
    
    Note
    ====

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,13 +4,16 @@ name="$2"
 image="$3"
 echo $name
 echo $image
+XAUTH=/tmp/.docker.xauth
 docker run --rm -d -it -v $1:/ws/ \
 			-v ./.ssh:/root/.ssh \
 			-v /dev:/dev \
     		--device-cgroup-rule "c 81:* rmw" \
     		--device-cgroup-rule "c 189:* rmw" \
             -e DISPLAY=$DISPLAY \
-            -v XAUTHORITY=$XAUTHORITY \
-            -v /tmp/.X11-unix:/tmp/.X11-unix \
-            --env="DISPLAY" --net=host \
-            --privileged --name $name $image bash
+            -e XAUTHORITY=$XAUTHORITY \
+			-e QT_GRAPHICSSYSTEM=native \
+			-v $XAUTH:$XAUTH:rw \
+			-v /tmp/.X11-unix:/tmp/.X11-unix \
+			--env="DISPLAY" --net=host \
+			--privileged --name $name $image bash
