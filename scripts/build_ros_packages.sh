c3c78ab9405a05f986a68b8ac426d9a9dfbbfbee
shandilya1998 <shandilya.shreyas@gmail.com>
2024-11-18 20:24:33 +0530

    Addition of MORB_SLAM in build scripts
    Problem
    =======
    1. Unable to download downstream SLAM packages due to missing MORB_SLAM
       dependencies
    2. Need to update ros2 package version
    
    Solution
    ========
    1. Added build and install instructions for MORB_SLAM using
       ros_ws/src/slam/ext/MORB_SLAM/build.sh
    2. Updated slam package to hash: 346d5d3
    
    Note
    ====
    MORB_SLAM may be a temporary feature due to possible bugs in code

--- scripts/build_ros_packages.sh
+++ scripts/build_ros_packages.sh
@@ -3,10 +3,15 @@
 export CMAKE_PREFIX_PATH=/Pangolin/build:$CMAKE_PREFIX_PATH
 export CMAKE_PREFIX_PATH=/opencv:$CMAKE_PREFIX_PATH
 
+
+
 echo "===============Building ROS2 Package=============="
 cd /ws/ros_ws
 echo "***********************************************"
 echo $PWD
+cd /ws/ros_ws/src/slam/ext/MORB_SLAM/
+./build.sh -DCMAKE_BUILD_TYPE=RELEASE -j1
+cd /ws/ros_ws
 echo "***********************************************"
 # rm -rf build install
 colcon build --symlink-install --cmake-args '-DCMAKE_EXPORT_COMPILE_COMMANDS=1' '-DCMAKE_BUILD_TYPE=Release' '-Wno-dev' --mixin debug
